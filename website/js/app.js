586fdc4c44db2366bc7e8aaa1b6e932a06bb3807
FalcoVH <Falco.van.haut@gmail.com>
2019-11-21 18:32:31 +0100

    website

---
+++ website/js/app.js
@@ -0,0 +1,14 @@
+$(document).foundation()
+$("[data-menu-underline-from-center] a").addClass("underline-from-center");
+$(function() {
+    $(window).scroll(function() {
+      var winTop = $(window).scrollTop();
+      if (winTop >= 100) {
+        $("body").addClass("sticky-shrinknav-wrapper");
+      } else{
+        $("body").removeClass("sticky-shrinknav-wrapper");
+      }
+    });
+  });
+  
+  
